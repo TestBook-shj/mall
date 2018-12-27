d73f24785f00c276bc5a604ea6dad7d9cf8cbaa9
TestBook-shj <704426676@qq.com>
2018-12-27 22:28:56 +0800

    通用header开发

---
+++ src/page/common/header/index.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: TestBook-shj
+ * @Date:   2018-12-27 21:48:50
+ * @Last Modified by:   TestBook-shj
+ * @Last Modified time: 2018-12-27 22:24:10
+ */
+'user strict';
+require('./index.css');
+var _mm = require('util/mm.js');
+// 通用页面头部
+var header = {
+  init: function() {
+    this.bindEvent();
+  },
+  onLoad: function() {
+    var keyword = _mm.getUrlParam('keyword');
+    // keyword存在，则回填输入框
+    if (keyword) {
+      $('#search-inport').val(keyword);
+    };
+  },
+  bindEvent: function() {
+    var _this = this;
+    // 点击搜索按钮以后，搜索提交
+    $('#search-btn').click(function() {
+      _this.searchSubmit();
+    });
+
+    // 输入回车，搜索提交
+    $('#search-input').keyup(function(e) {
+      // 13是回车键的keyCode
+      if (e.keyCode === 13) {
+        _this.searchSubmit();
+      }
+    })
+  },
+  // 搜索提交
+  searchSubmit: function() {
+    var keyword = $.trim($('#search-input').val());
+    // 如果提交的时候有值，跳转到list页
+    if (keyword) {
+      window.location.href = './list.html?keyword=' + keyword;
+    }
+    // 如果keyword为空，直接返回首页
+    else {
+      _mm.goHome();
+    }
+  }
+};
+header.init();
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,11 +1,12 @@
 /*
  * @Author: TestBook-shj
  * @Date:   2018-12-21 21:36:38
- * @Last Modified by:   Administrator
- * @Last Modified time: 2018-12-27 17:19:52
+ * @Last Modified by:   TestBook-shj
+ * @Last Modified time: 2018-12-27 22:27:03
  */
 
 'use strict'
 require('page/common/nav/index.js');
+require('page/common/header/index.js');
 var _mm = require('util/mm.js');
 
