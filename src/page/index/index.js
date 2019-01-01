193094a665da0868521991241b7c4d524c06f842
TestBook-shj <704426676@qq.com>
2019-01-01 17:33:30 +0800

    楼层界面开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,14 +2,11 @@
  * @Author: TestBook-shj
  * @Date:   2018-12-21 21:36:38
  * @Last Modified by:   TestBook-shj
- * @Last Modified time: 2018-12-28 21:11:11
+ * @Last Modified time: 2019-01-01 15:47:37
  */
 'use strict'
+require('./index.css')
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
 var navSide = require('page/common/nav-side/index.js');
 var _mm = require('util/mm.js');
-
-navSide.init({
-  name: 'user-center'
-});
