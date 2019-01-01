635b1565a13a83bd23d4d9b2f8d38d036308cc73
TestBook-shj <704426676@qq.com>
2019-01-01 22:21:53 +0800

    首页开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,11 +2,28 @@
  * @Author: TestBook-shj
  * @Date:   2018-12-21 21:36:38
  * @Last Modified by:   TestBook-shj
- * @Last Modified time: 2019-01-01 15:47:37
+ * @Last Modified time: 2019-01-01 22:20:08
  */
 'use strict'
-require('./index.css')
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
 var navSide = require('page/common/nav-side/index.js');
+var templateBanner = require('./banner.string');
 var _mm = require('util/mm.js');
+
+$(function() {
+  // 渲染banner的html
+  var bannerHtml = _mm.renderHtml(templateBanner);
+  $('.banner-con').html(bannerHtml);
+  // 初始化banner
+  var $slider = $('.banner').unslider({
+    dots: true
+  });
+  // 前一张和后一张操作的事件绑定
+  $('.banner-con .banner-arrow').click(function(){
+    var forward = $(this).hassClass('prev') ? 'prev' : 'next';
+    $slider.data('unslider')[forward]();
+  })
+});
